0629f98a5b633b04a18232711e9d577d66f72ae2
wardenlym <wardenlym@gmail.com>
2017-07-04 13:25:18 +0800

    struct

--- config.go
+++ config.go
@@ -4,22 +4,16 @@ type Config interface {
 	Map() map[string]interface{}
 }
 
-type CounterConfig struct {
-	data []byte
-}
-
-func (p *CounterConfig) Map() interface{} {
-	return nil
-}
-
 type accumulator_config struct {
-	RedisAddress string
-	RedisSetname string
+	RedisAddress  string
+	RedisPassword string
+	RedisDB       string
 
-	MysqlAddress string
-	MysqlDB      string
-	MysqlTable   string
-	MysqlField   string
+	MysqlAddress  string
+	MysqlPassword string
+	MysqlDB       string
+	MysqlTable    string
+	MysqlField    string
 
 	FlushIntervalSecond int
 	MaxKeyCached        int
---
+++ reids_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func Test_redis(t *testing.T) {
+	test()
+}
