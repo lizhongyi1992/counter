1969a6b327512a5373f7e0491ddbc59f9f217d0b
wardenlym <wardenlym@gmail.com>
2017-07-04 12:24:23 +0900

    add redis

--- cache.go
+++ cache.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/go-redis/redis"
+)
+
 type accumulator struct {
 	config accumulator_config
+}
+
+func test() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+
+	})
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
 }
